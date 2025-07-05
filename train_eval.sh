49f9f2bf84830bcc9308b096554cd377965f281d
Jinx-Li <scintigimcki@outlook.com>
2025-07-05 16:23:54 +0800

    update all the hyperparameters

--- train_eval.sh
+++ train_eval.sh
@@ -44,42 +44,42 @@ exp=bat
 ################################################################## ParticleNerf
 output=output/particle_nerf
 exp=pendulums
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75 --fps 60
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --fps 60 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 60
 #python metrics.py -m $output/$exp --s test --half_res
 #python metrics.py -m $output/$exp --s val --half_res
 #python render.py -m $output/$exp --mode all --skip_val --skip_test --fps 60
 
 exp=cloth
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75 --fps 60
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --fps 60 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 60
 #python metrics.py -m $output/$exp --s test --half_res
 #python metrics.py -m $output/$exp --s val --half_res
 #python render.py -m $output/$exp --mode all --skip_val --skip_test --fps 60
 
 exp=spring
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75 --fps 60
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --fps 60 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 60
 #python metrics.py -m $output/$exp --s test --half_res
 #python metrics.py -m $output/$exp --s val --half_res
 #python render.py -m $output/$exp --mode all --skip_val --skip_test --fps 60
 
 exp=robot
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75 --fps 30
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --fps 30 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 30
 #python metrics.py -m $output/$exp --s test --half_res
 #python metrics.py -m $output/$exp --s val --half_res
 #python render.py -m $output/$exp --mode all --skip_val --skip_test
 
 exp=robot-task
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75 --fps 30
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --fps 30 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 30
 #python metrics.py -m $output/$exp --s test --half_res
 #python metrics.py -m $output/$exp --s val --half_res
 #python render.py -m $output/$exp --mode all --skip_val --skip_test
 
 exp=wheel
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75 --fps 60
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --fps 60 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 60
 #python metrics.py -m $output/$exp --s test --half_res
 #python metrics.py -m $output/$exp --s val --half_res
@@ -120,14 +120,14 @@ exp=factory
 ################################################################## NVIDIA
 output=output/NVIDIA_dynamic_scene
 exp=Skating
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.65 --fps 20 --vel_start_time 0.35
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.65 --fps 20 --vel_start_time 0.35 --physics_code 16
 ##python seg.py -m $output/$exp --K 4 --vis
 #python render.py -m $output/$exp --mode render --skip_train --fps 20
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 
 exp=Truck
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.65 --fps 60
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.65 --fps 60 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 60
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
@@ -135,45 +135,45 @@ exp=Truck
 ################################################################## GoPro
 output=output/GOPRO
 exp=pen1
-#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.70
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.70 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 #python render.py -m $output/$exp --mode original --skip_val --skip_test
 
 exp=box
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.70
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.70 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 
 exp=mat
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.75
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.75 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train --fps 88
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 
 exp=collision
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.73
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.73 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 #python render.py -m $output/$exp --mode original --skip_val --skip_test
 
 exp=cube
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.72
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.72 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 
 exp=hammer
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.70
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.70 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
 
 exp=pen2
-#python train_gui.py -s /path/to/data/ -m $output/$exp --eval --is_blender --max_time 0.70
+#python train_gui.py -s /path/to/data/ -m $output/$exp --max_time 0.70 --physics_code 16
 #python render.py -m $output/$exp --mode render --skip_train
 #python metrics.py -m $output/$exp --s test
 #python metrics.py -m $output/$exp --s val
